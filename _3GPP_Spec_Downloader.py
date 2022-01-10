67f0f7a48bd3d1b8ae38d01cf1e42a02fac54727
yakosgit <yako.k.yako@gmail.com>
2022-01-09 21:43:30 -0500

    quick clean up
    improve renaming function by correcting matching file search logic

--- _3GPP_Spec_Downloader.py
+++ _3GPP_Spec_Downloader.py
@@ -1,25 +1,30 @@
+
+#################################################################################################
+#
+#!/usr/bin/env python
+# coding: utf-8
+#
 #################################################################################################
 #
 # Author: Yako K. Yako
 #
 #################################################################################################
-# Script to automatically download, rename, and display the title the URL of a requested 
-# 3GPP spec doc number (e.g 16.38.101)
 #
+# Script to automatically download, rename, and display the title and the URL of a requested 
+# 3GPP spec doc number (e.g 16.38.101)
 # HOW TO USE:
 # 1- change the "path" variable in the script to a proper path (e.g. path=r"C:\Users\YOURPC\Desktop\3GPP/")
 # 2- place the file "3GPP_Spec_Titles.xlsx" into the path you choose (optional) 
 #    - needed in order to return the title of any spec doc# you enter and rename the downloaded files
 # 3- Run command: python _3GPP_Spec_Downloader.py arg1 arg2 (e.g. python _3GPP_Spec_Downloader.py true true)
-#    The 2 arguments (arg1 arg2) are flags for download and rename options respectively
+#    The 2 arguments (arg1 arg2) are flags for download and rename options, respectively
 #    true  true   -> download and rename (e.g 16.38.201 will be saved as "38201-g00-NR; Physical layer; General description-Rel-16")
 #    true  false  -> download and keep original file name ( default name is 3GPP spec number, e.g 38201-g00-NR) 
 #    false false  -> no download or rename, just display the title of the spec doc number
 #    false true   -> no download or rename, just display the title of the spec doc number
-# 4- when prompeted "Enter desired 3GPP release.series.doc# (e.g 16.38.101): ", enter the info of the doc you need
+# 4- when prompeted "Enter desired 3GPP release.series.doc# (e.g. 16.38.101): ", enter the info of the doc you need
 #**note:if you don't provide doc# (e.g 16.38.) the script will download all docs in that series**
 # 5- ENJOY :)
-#
 #################################################################################################
 
 import os
@@ -36,14 +41,14 @@ from bs4 import BeautifulSoup as bs
 #################################################################################################
 
 # path where spec files will be downloaded and where "3GPP_Spec_Titles.xlsx" file is located
-path=r"C:\Users\YOURPC\Desktop\3GPP/"
-# path where you want to save 3gpp spec files
-where_to_save = path+r'\3gpp_spec_files'
+path=r"C:\Users\Yako K. Yako\Desktop\3GPP/"
 # read sys argv for download and rename options
 download = sys.argv[1].lower() == 'true'
 rename = sys.argv[2].lower() == 'true'
 # reading user input of desired 3gpp doc to download 
 _3GPP_Release, _3GPP_Series, _3GPP_Doc_Num = input("Enter desired 3GPP release.series.doc# (e.g 16.38.101): ").split('.') 
+# path where you want to save 3gpp spec files
+where_to_save = path+r'\3gpp_spec_files\Rel_'+_3GPP_Release
 
 # func to retrieve full spec title from a spec number
 def get_3gpp_spec_titles(_3GPP_Series, _3GPP_Doc_Num):
@@ -112,7 +117,7 @@ if download and match_link_list:
 if download and rename and match_link_list and spec_titles:
     print('renaming')
     for filename in os.listdir('.'):
-        _3GPP_Spec_Num = re.match(r'(.*)-(g.*).(doc.*)', filename)
+        _3GPP_Spec_Num = re.match(r'(.*)-(.*).(doc.*)', filename)
         try:
             if _3GPP_Spec_Titles['spec_number_cln'].str.contains(_3GPP_Spec_Num.group(1)).any():
                 title = _3GPP_Spec_Titles[_3GPP_Spec_Titles['spec_number_cln'].str.contains(_3GPP_Spec_Num.group(1))].title.values[0]
